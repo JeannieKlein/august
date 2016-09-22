7f82a6538f89ce477105f30b725dcc2dc723c028
Jeannie Klein <kleinje@i21a-s1.ufhpc>
2016-09-22 12:18:13 -0400

    find high temp for each day in august of every year

--- HighTemp.sh
+++ HighTemp.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-# Script to get high temperature for each day in August  between 1973 and 2016
+# Script to get high temperature for each day in August between 1973 and 2016
 #
 #Script by Jeannie
-for file in {/ufrc/zoo6927/kleinje/data/}
+for file in /ufrc/zoo6927/kleinje/data/*/*.txt
 do
-HighTemp= awk -F',' '{print $2}' $file | sort -n | tail -n1
-
-#echo "Getting highest temperature for $file"
+MaxTemp= awk -F',' '{print $2}' $file | sort -n | tail -n1
+echo "Getting highest temperature for $file"
 done
