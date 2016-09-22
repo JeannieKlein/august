a3d73f635e52516e03b9320e6e786496e7940d12
Jeannie Klein <kleinje@i21a-s1.ufhpc>
2016-09-22 13:44:06 -0400

    find 5 max and 5 low temps for august over years

--- getHighTemp.sh
+++ getHighTemp.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 #Script to get the weather from WeatherUnderground for August between 1943 and 2016
 #
-#Script by Matt 
-
-month=08 # Only looking at August.
+#Script by Jeannie, stolen from Matt
 
 for year in {1973..2016} #For each year between 1943 and 2016
 do
@@ -11,5 +9,9 @@ do
   do
     MaxTemp=`awk -F',' '{print $2}' $file | sort -n | tail -n1`
     echo "$year $MaxTemp" >> $year.Max.txt
- done
+   done
+   sort -k2n $year.Max.txt | tail -n1 >> annual.Max.txt
+
 done
+sort -k2n annual.Max.txt | tail -n5 >> FiveMaxYears.txt
+sort -k2n annual.Max.txt | head -n5 >> FiveLowYears.txt
